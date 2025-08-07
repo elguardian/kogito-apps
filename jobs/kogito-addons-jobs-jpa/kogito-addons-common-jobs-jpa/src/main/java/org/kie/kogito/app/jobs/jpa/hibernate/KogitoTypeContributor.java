f29a6c23cb0b4a358228a37a39997067a9340d8a
Enrique Gonzalez Martinez <Enrique.Gonzalez.Martinez1@ibm.com>
2025-08-07 09:30:40 +0200

    [incubator-kie-issues-2049] Custom collocated job service for bpmn2 and springboot support

---
+++ jobs/kogito-addons-jobs-jpa/kogito-addons-common-jobs-jpa/src/main/java/org/kie/kogito/app/jobs/jpa/hibernate/KogitoTypeContributor.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.kie.kogito.app.jobs.jpa.hibernate;
+
+import org.hibernate.boot.model.TypeContributions;
+import org.hibernate.boot.model.TypeContributor;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.PostgreSQLDialect;
+import org.hibernate.dialect.PostgreSQLJsonPGObjectJsonbType;
+import org.hibernate.engine.jdbc.spi.JdbcServices;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.type.SqlTypes;
+import org.hibernate.usertype.UserTypeSupport;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class KogitoTypeContributor implements TypeContributor {
+
+    @Override
+    public void contribute(TypeContributions typeContributions, ServiceRegistry serviceRegistry) {
+        JdbcServices jdbcServices = serviceRegistry.getService(JdbcServices.class);
+        Dialect dialect = jdbcServices.getDialect();
+        // only register if we have the postgresql support
+        UserTypeSupport<ObjectNode> userTypeSupport;
+        if (dialect instanceof PostgreSQLDialect) {
+            typeContributions.getTypeConfiguration().getJdbcTypeRegistry().addDescriptor(SqlTypes.JSON, new PostgreSQLJsonPGObjectJsonbType());
+            userTypeSupport = new JsonBinaryUserType();
+        } else {
+            userTypeSupport = new JsonStringUserType();
+        }
+        userTypeSupport.setTypeConfiguration(typeContributions.getTypeConfiguration());
+        typeContributions.contributeType(userTypeSupport);
+    }
+
+}
---
+++ jobs/kogito-addons-quarkus-jobs/src/main/java/org/kie/kogito/app/jobs/quarkus/QuarkusJobsService.java
@@ -0,0 +1,151 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.kie.kogito.app.jobs.quarkus;
+
+import java.util.concurrent.Callable;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.kie.kogito.app.jobs.api.JobExecutor;
+import org.kie.kogito.app.jobs.api.JobScheduler;
+import org.kie.kogito.app.jobs.api.JobSchedulerBuilder;
+import org.kie.kogito.app.jobs.api.JobSchedulerListener;
+import org.kie.kogito.app.jobs.api.JobTimeoutInterceptor;
+import org.kie.kogito.app.jobs.integregations.ProcessInstanceJobDescriptionJobInstanceEventAdapter;
+import org.kie.kogito.app.jobs.integregations.ProcessJobDescriptionJobInstanceEventAdapter;
+import org.kie.kogito.app.jobs.integregations.UserTaskInstanceJobDescriptionJobInstanceEventAdapter;
+import org.kie.kogito.app.jobs.spi.JobContextFactory;
+import org.kie.kogito.app.jobs.spi.JobStore;
+import org.kie.kogito.event.EventPublisher;
+import org.kie.kogito.jobs.JobDescription;
+import org.kie.kogito.jobs.JobsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.narayana.jta.QuarkusTransaction;
+import io.quarkus.runtime.Startup;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.transaction.Transactional;
+
+@Transactional
+@Singleton
+@Startup
+public class QuarkusJobsService implements JobsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(QuarkusJobsService.class);
+
+    protected JobScheduler jobScheduler;
+
+    @Inject
+    protected Instance<JobExecutor> jobExecutors;
+
+    @Inject
+    protected Instance<EventPublisher> eventPublisher;
+
+    @Inject
+    protected JobStore jobStore;
+
+    @Inject
+    protected JobContextFactory jobContextFactory;
+
+    @Inject
+    protected Instance<JobSchedulerListener> jobSchedulerListeners;
+
+    @ConfigProperty(name = "kogito.jobs-service.maxNumberOfRetries", defaultValue = "3")
+    protected Integer maxNumberOfRetries;
+
+    @ConfigProperty(name = "kogito.jobs-service.maxIntervalLimitToRetryMillis", defaultValue = "60000")
+    protected Long maxIntervalLimitToRetryMillis;
+
+    @ConfigProperty(name = "kogito.jobs-service.schedulerChunkInMinutes", defaultValue = "10")
+    protected Long maxRefreshJobsIntervalWindow;
+
+    @PostConstruct
+    public void init() {
+        JobTimeoutInterceptor txInterceptor = new JobTimeoutInterceptor() {
+
+            @Override
+            public Callable<Void> chainIntercept(Callable<Void> callable) {
+                return new Callable<Void>() {
+
+                    @Override
+                    public Void call() throws Exception {
+                        return QuarkusTransaction.requiringNew().call(callable);
+                    }
+
+                };
+            }
+        };
+        this.jobScheduler = JobSchedulerBuilder.newJobSchedulerBuilder()
+                .withEventPublishers(eventPublisher.stream().toArray(EventPublisher[]::new))
+                .withJobSchedulerListeners(jobSchedulerListeners.stream().toArray(JobSchedulerListener[]::new))
+                .withJobStore(jobStore)
+                .withJobContextFactory(jobContextFactory)
+                .withJobEventAdapters(
+                        new ProcessInstanceJobDescriptionJobInstanceEventAdapter(),
+                        new ProcessJobDescriptionJobInstanceEventAdapter(),
+                        new UserTaskInstanceJobDescriptionJobInstanceEventAdapter())
+                .withJobExecutors(jobExecutors.stream().toArray(JobExecutor[]::new))
+                .withMaxRefreshJobsIntervalWindow(maxRefreshJobsIntervalWindow * 60 * 1000L)
+                .withRetryInterval(maxIntervalLimitToRetryMillis)
+                .withMaxNumberOfRetries(maxNumberOfRetries)
+                .withRefreshJobsInterval(maxRefreshJobsIntervalWindow * 60 * 1000L)
+                .withTimeoutInterceptor(txInterceptor)
+                .build();
+        this.jobScheduler.init();
+
+        LOG.info("Initializing Job Service Logic \n" +
+                "MaxRefreshJobsIntervalWindow: {} (millis)\n" +
+                "MaxIntervalLimitToRetryMillis: {} (millis)\n" +
+                "MaxNumberOfRetries: {}\n" +
+                "RefreshJobsInterval: {} (millis)\n" +
+                "Store: {}",
+                maxRefreshJobsIntervalWindow * 60 * 1000L,
+                maxIntervalLimitToRetryMillis,
+                maxNumberOfRetries,
+                maxRefreshJobsIntervalWindow * 60 * 1000L,
+                jobStore);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        this.jobScheduler.close();
+    }
+
+    @Override
+    public String scheduleJob(JobDescription jobDescription) {
+        return jobScheduler.schedule(jobDescription);
+    }
+
+    @Override
+    public boolean cancelJob(String jobId) {
+        jobScheduler.cancel(jobId);
+        return true;
+    }
+
+    @Override
+    public String rescheduleJob(JobDescription jobDescription) {
+        return jobScheduler.reschedule(jobDescription);
+    }
+
+}
